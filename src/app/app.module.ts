53c672ccea31276903b0e7a3db78755a567f5589
Alen Thankachan <alenthankz@gmail.com>
2020-06-21 07:57:52 -0700

    finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { ContetSideNavComponent } from './sidenav/contet-side-nav/contet-side-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidenavComponent,
+    ContetSideNavComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/sidenav/contet-side-nav/contet-side-nav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contet-side-nav',
+  templateUrl: './contet-side-nav.component.html',
+  styleUrls: ['./contet-side-nav.component.css']
+})
+export class ContetSideNavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
